bfa9ccc4a0f62937c56752c3cbc43389a4924072
atlwendy <atlwendy@gmail.com>
2020-06-21 11:02:26 -0400

    testing out the changes

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,21 @@ import * as github from '@actions/github'
 import * as Message from './message-retrieve'
 
 async function run(): Promise<any> {
-  const payload = github.context.payload
   const token = core.getInput('token')
-  const branch = payload.pull_request.head.ref
-  const repoName = payload.repository.name
-  const repo = payload.repository.clone_url
+  const userWithRepo = process.env.GITHUB_REPOSITORY
+  const ref = process.env.GITHUB_REF
+
+  const repoName = userWithRepo.split('/')[1]
+  const branch = ref.replace('refs/heads/', '')
+  const repoUrl = `https://github.com/${userWithRepo}.git`
   try {
-    if (branch && repo) {
+    if (branch && repoUrl) {
       const message = new Message.MessageRetrieved(
         branch,
         repoName,
-        repo,
+        repoUrl,
         token
       )
-      return message.execute()
     }
   } catch (error) {
     core.setFailed(error.message)
